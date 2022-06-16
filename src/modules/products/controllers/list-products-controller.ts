86008bc9ddba0ba236ef20514e0feb95d89f81a8
Rian Marlon <rianmarlon20@yahoo.com>
2022-06-16 15:59:39 -0300

    feat: add list products controller

---
+++ src/modules/products/controllers/list-products-controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+import ListProductsService from '../services/list-products-service';
+
+class ListProductsController {
+  async handleRequest(request: Request, response: Response) {
+    const listProductsService = new ListProductsService();
+    const products = await listProductsService.execute();
+
+    return response.status(200).json(products);
+  }
+}
+
+export default ListProductsController;
