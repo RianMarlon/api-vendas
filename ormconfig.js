4642b1a1752f1229ba71b65b9b6b3c7a8939c327
Rian Marlon <rianmarlon20@yahoo.com>
2022-06-16 17:07:16 -0300

    chore: add entities path in ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,7 @@ module.exports = {
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
+  entities: ["./src/modules/**/typeorm/entities/*.ts"],
   migrations: ["./src/shared/typeorm/migrations/*.ts"],
   cli: {
     migrationsDir: "./src/shared/typeorm/migrations"
