9645d2548b73fa298c1ad61c54a4627eee235e08
Rian Marlon <rianmarlon20@yahoo.com>
2022-06-19 17:17:13 -0300

    feat: add user entity

---
+++ src/modules/users/typeorm/entities/user.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { UUID } from '@shared/utils/uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  constructor() {
+    const uuid = new UUID();
+
+    if (!this.id) {
+      this.id = uuid.generate();
+    }
+  }
+}
+
+export default User;
