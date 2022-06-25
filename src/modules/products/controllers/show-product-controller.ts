e88d474b8eb81254a59c4767feb32f24c5d6a554
Rian Marlon <rianmarlon20@yahoo.com>
2022-06-25 20:04:36 -0300

    refactor: change show product controller

--- src/modules/products/controllers/show-product-controller.ts
+++ src/modules/products/controllers/show-product-controller.ts
@@ -1,16 +1,10 @@
 import { Request, Response } from 'express';
 
-import AppError from '@shared/errors/app-error';
-
 import ShowProductService from '../services/show-product-service';
 
 class ShowProductController {
   async handleRequest(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
-
-    if (!id) {
-      throw new AppError('ID não informado');
-    }
 
     const showProductService = new ShowProductService();
     const product = await showProductService.execute(id);
