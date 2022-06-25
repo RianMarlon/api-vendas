9ad2958aaa3487e32279856dc3d81ca568b27fa0
Rian Marlon <rianmarlon20@yahoo.com>
2022-06-25 20:27:05 -0300

    refactor: change update product controller

--- src/modules/products/controllers/update-product-controller.ts
+++ src/modules/products/controllers/update-product-controller.ts
@@ -1,17 +1,11 @@
 import { Request, Response } from 'express';
 
-import AppError from '@shared/errors/app-error';
-
 import UpdateProductService from '../services/update-product-service';
 
 class UpdateProductController {
   async handleRequest(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
     const { name, price, quantity } = request.body;
-
-    if (!id) {
-      throw new AppError('ID não informado');
-    }
 
     const updateProductService = new UpdateProductService();
     const product = await updateProductService.execute(id, {
