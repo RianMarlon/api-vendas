320aff66890f2da50414fb3ef96cfc3cf0440f16
Rian Marlon <rianmarlon20@yahoo.com>
2022-07-07 20:09:45 -0300

    feat: add user token entity

---
+++ src/modules/users/typeorm/entities/user-token.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { UUID } from '@shared/utils/uuid';
+
+@Entity('users_tokens')
+class UserToken {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  token: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  constructor() {
+    const uuid = new UUID();
+
+    if (!this.id) {
+      this.id = uuid.generate();
+    }
+
+    if (!this.token) {
+      this.token = uuid.generate();
+    }
+  }
+}
+
+export default UserToken;
