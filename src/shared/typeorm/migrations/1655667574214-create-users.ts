8384192887442727ee177769a02d71282e499476
Rian Marlon <rianmarlon20@yahoo.com>
2022-07-07 19:59:57 -0300

    refactor: change migration class name to create table users

--- src/shared/typeorm/migrations/1655667574214-create-users.ts
+++ src/shared/typeorm/migrations/1655667574214-create-users.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class createUsers1655667574214 implements MigrationInterface {
+export class CreateUsers1655667574214 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
