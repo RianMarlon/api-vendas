69fe05c9e4172361fa4085622d9f6840fef3dbf3
Rian Marlon <rianmarlon20@yahoo.com>
2022-07-07 19:40:26 -0300

    feat: add route to view images uploaded

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 import AppError from '@shared/errors/app-error';
 
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
