ea84d3609bb8445047b7446309922df438da81ce
Rian Marlon <rianmarlon20@yahoo.com>
2022-07-10 10:46:47 -0300

    feat: add reset password service

---
+++ src/modules/users/services/reset-password-service.ts
@@ -0,0 +1,49 @@
+import { isAfter, isBefore, addHours } from 'date-fns';
+
+import AppError from '@shared/errors/app-error';
+import Hash from '@shared/utils/hash';
+
+import UsersRepository from '../typeorm/repositories/users-repository';
+import UsersTokensRepository from '../typeorm/repositories/users-tokens-repository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  async execute(data: IRequest): Promise<void> {
+    const usersRepository = new UsersRepository();
+    const usersTokensRepository = new UsersTokensRepository();
+
+    const userTokenByToken = await usersTokensRepository.findByToken(
+      data.token,
+    );
+
+    if (!userTokenByToken) {
+      throw new AppError('User token does not exists.');
+    }
+
+    const userById = await usersRepository.findById(userTokenByToken.userId);
+
+    if (!userById) throw new AppError('User does not exists');
+
+    const tokenCreatedAt = userTokenByToken.createdAt;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (
+      isAfter(Date.now(), compareDate) ||
+      isBefore(Date.now(), tokenCreatedAt)
+    )
+      throw new AppError('Token expired');
+
+    const hash = new Hash();
+    const newPassword = await hash.generate(data.password);
+
+    await usersRepository.update(userById.id, {
+      password: newPassword,
+    });
+  }
+}
+
+export default ResetPasswordService;
