d68c86ac1d75d89765e794475d2f3d35db258630
Rian Marlon <rianmarlon20@yahoo.com>
2022-11-02 20:33:03 -0300

    feat: delete customer service

---
+++ src/modules/customers/services/delete-customer-service.ts
@@ -0,0 +1,16 @@
+import AppError from '@shared/errors/app-error';
+
+import CustomersRepository from '../typeorm/repositories/customers-repository';
+
+class DeleteCustomerService {
+  async execute(id: string): Promise<void> {
+    const customersRepository = new CustomersRepository();
+    const customerById = await customersRepository.findById(id);
+
+    if (!customerById) throw new AppError('Customer not found');
+
+    await customersRepository.delete(id);
+  }
+}
+
+export default DeleteCustomerService;
