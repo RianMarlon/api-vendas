6c2d8cfd9b7c55168d06fb28e03906f55344a8e8
Rian Marlon <rianmarlon20@yahoo.com>
2022-11-02 20:14:47 -0300

    feat: add create customer service

---
+++ src/modules/customers/services/create-customer-service.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/app-error';
+
+import Customer from '../typeorm/entities/customer';
+
+import CustomersRepository from '../typeorm/repositories/customers-repository';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateCustomerService {
+  async execute(data: IRequest): Promise<Customer> {
+    data.email = data.email?.toLowerCase();
+
+    const customersRepository = new CustomersRepository();
+    const customerByEmail = await customersRepository.findByEmail(data.email);
+
+    if (customerByEmail) throw new AppError('Email address already used');
+
+    const customerCreated = new Customer();
+    customerCreated.name = data.name;
+    customerCreated.email = data.email.toLowerCase();
+
+    await customersRepository.create(customerCreated);
+
+    return customerCreated;
+  }
+}
+
+export default CreateCustomerService;
