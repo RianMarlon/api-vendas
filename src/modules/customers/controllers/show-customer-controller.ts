ad4a7c0f7b23816a628e12507477fcab9e2162a4
Rian Marlon <rianmarlon20@yahoo.com>
2022-11-02 20:52:00 -0300

    feat: add show customer controller

---
+++ src/modules/customers/controllers/show-customer-controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import ShowCustomerService from '../services/show-customer-service';
+
+class ShowCustomerController {
+  async handleRequest(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showCustomerService = new ShowCustomerService();
+    const customer = await showCustomerService.execute(id);
+
+    return response.status(200).json(customer);
+  }
+}
+
+export default ShowCustomerController;
