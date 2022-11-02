e2d2850796b23f0131bffbf6f488769abe5829ff
Rian Marlon <rianmarlon20@yahoo.com>
2022-11-02 20:41:16 -0300

    feat: add create customer controller

---
+++ src/modules/customers/controllers/create-customer-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+import CreateCustomerService from '../services/create-customer-service';
+
+class CreateCustomerController {
+  async handleRequest(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+
+    const createCustomerService = new CreateCustomerService();
+    const customer = await createCustomerService.execute({
+      name,
+      email,
+    });
+
+    return response.status(201).json(customer);
+  }
+}
+
+export default CreateCustomerController;
