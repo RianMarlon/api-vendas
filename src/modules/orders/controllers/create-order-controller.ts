aa859fb660c152cc4d82da45cc4c7b4f43d20b58
Rian Marlon <rianmarlon20@yahoo.com>
2022-11-06 16:17:13 -0300

    feat: add create order controller

---
+++ src/modules/orders/controllers/create-order-controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+
+import CreateOrderService from '../services/create-order-service';
+
+class CreateOrderController {
+  async handleRequest(request: Request, response: Response): Promise<Response> {
+    const { customerId, products } = request.body;
+
+    const createOrderService = new CreateOrderService();
+    const order = await createOrderService.execute({
+      customerId,
+      products,
+    });
+
+    return response.status(201).json(order);
+  }
+}
+
+export default CreateOrderController;
