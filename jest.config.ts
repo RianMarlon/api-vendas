665e1a485f6da39e06b8453f610eef01b1d58c72
Rian Marlon <rianmarlon20@yahoo.com>
2023-01-22 15:35:56 -0300

    chore: add collect coverage in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import { compilerOptions } from './tsconfig.json';
 
 export default {
   clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
   coverageProvider: 'v8',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
